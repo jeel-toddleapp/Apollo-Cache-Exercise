c7b0b59a6dbbb70f56423634d50253fa622cf1fe
Jeel2308 <jeel@toddleapp.com>
2024-03-21 18:06:51 +0530

    chore: remove unused schema

--- src/server/schema.js
+++ src/server/schema.js
@@ -9,38 +9,15 @@ import {
 
 import { v4 as uuidv4 } from 'uuid';
 
-import {
-  tags,
-  books,
-  authors,
-  tagsByBook,
-  additionalDetailsByBook,
-} from './data';
+import { books, additionalDetailsByBook } from './data';
 
 import _ from 'lodash';
-
-const AuthorType = new GraphQLObjectType({
-  name: 'Author',
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    email: { type: GraphQLString },
-    books: {
-      type: new GraphQLList(BookType),
-      resolve: (author) => {
-        return books.filter((book) => book.authorId === author.id);
-      },
-    },
-  }),
-});
 
 const AdditionalDetailsByBook = new GraphQLObjectType({
   name: 'AdditionalDetailsByBook',
   fields: () => ({
     id: { type: GraphQLID },
     year: { type: GraphQLString },
-    pricePerUnit: { type: GraphQLString },
-    totalSell: { type: GraphQLString },
   }),
 });
 
@@ -48,17 +25,7 @@ const AdditionalDetailsByBookInputType = new GraphQLInputObjectType({
   name: 'AdditionalDetailsByBookInput',
   fields: () => ({
     year: { type: GraphQLString },
-    pricePerUnit: { type: GraphQLString },
-    totalSell: { type: GraphQLString },
   }),
-});
-
-const TagType = new GraphQLObjectType({
-  name: 'Tag',
-  fields: {
-    id: { type: GraphQLID },
-    value: { type: GraphQLString },
-  },
 });
 
 const BookType = new GraphQLObjectType({
@@ -68,36 +35,7 @@ const BookType = new GraphQLObjectType({
       type: GraphQLID,
     },
     name: { type: GraphQLString },
-    author: {
-      type: AuthorType,
-      resolve: (book) => {
-        const author = authors.find((author) => author.id === book.authorId);
 
-        if (!author) {
-          return null;
-        }
-
-        return author;
-      },
-    },
-    tag: {
-      type: TagType,
-      resolve: (book) => {
-        const tag = tags.find((tag) => tag.id === book.tagId);
-
-        if (!tag) {
-          return null;
-        }
-
-        return tag;
-      },
-    },
-    tags: {
-      type: new GraphQLList(TagType),
-      resolve: (book) => {
-        return tagsByBook[book.id];
-      },
-    },
     additionalDetailsByBook: {
       type: new GraphQLList(AdditionalDetailsByBook),
       resolve: (book) => {
@@ -117,10 +55,6 @@ const QueryType = new GraphQLObjectType({
           return books;
         },
       },
-      authors: {
-        type: new GraphQLList(AuthorType),
-        resolve: () => authors,
-      },
     };
   },
 });
@@ -128,63 +62,11 @@ const QueryType = new GraphQLObjectType({
 const MutationType = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    updateAuthor: {
-      type: AuthorType,
-      args: {
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        email: { type: GraphQLString },
-      },
-      resolve: (root, args) => {
-        const author = authors.find((author) => author.id === args.id);
-        author.name = args.name || author.name;
-        author.email = args.email || author.email;
-        return author;
-      },
-    },
-    createBook: {
-      type: BookType,
-      args: {
-        name: { type: GraphQLString },
-        authorId: { type: GraphQLID },
-        tagId: { type: GraphQLID },
-      },
-      resolve: (root, args) => {
-        const book = {
-          id: uuidv4(),
-          name: args.name,
-          author: args.authorId,
-          tagId: args.tagId,
-          __typename: 'Book',
-        };
-        books.push(book);
-        return book;
-      },
-    },
-    createAuthor: {
-      type: AuthorType,
-      args: {
-        name: { type: GraphQLString },
-        email: { type: GraphQLString },
-      },
-      resolve: (root, args) => {
-        const author = {
-          id: authors.length + 1,
-          name: args.name,
-          email: args.email,
-          __typename: 'Author',
-        };
-        authors.push(author);
-        return author;
-      },
-    },
     updateBook: {
       type: BookType,
       args: {
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        authorId: { type: GraphQLID },
-        tags: { type: new GraphQLList(GraphQLID) },
         additionalDetails: {
           type: new GraphQLList(AdditionalDetailsByBookInputType),
         },
@@ -192,11 +74,6 @@ const MutationType = new GraphQLObjectType({
       resolve: (root, args) => {
         const book = books.find((book) => book.id === args.id);
         book.name = args.name || book.name;
-        book.authorId = args.authorId || book.authorId;
-
-        if (args.tags) {
-          tagsByBook[book.id] = args.tags;
-        }
 
         if (args.additionalDetails) {
           const additionalDetailsFormatted = args.additionalDetails.map(
