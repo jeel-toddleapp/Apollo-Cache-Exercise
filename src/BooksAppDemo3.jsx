d08a9f6be242937de0e5e554e6e930fff2a4a7ba
Jeel2308 <jeel@toddleapp.com>
2024-03-21 16:20:28 +0530

    chore: remove unrelated features

--- src/BooksAppDemo3.jsx
+++ src/BooksAppDemo3.jsx
@@ -4,7 +4,6 @@ import { withLoader, Book } from './components/index.js';
 import { useMutation } from '@apollo/client/react/hooks';
 import { composeHocs } from './utils.js';
 import React from 'react';
-import AuthorsFeedWrapper from './AuthorsFeeds.js';
 
 import client from './client.js';
 
@@ -17,30 +16,6 @@ const booksQuery = gql`
         id
         name
         email
-      }
-      additionalDetailsByBook {
-        id
-        year
-        pricePerUnit
-        totalSell
-      }
-    }
-  }
-`;
-
-const booksQueryWithError = gql`
-  query getBooksWithError {
-    books {
-      id
-      name
-      author {
-        id
-        name
-        email
-      }
-      additionalDetailsByBook {
-        id
-        year
       }
     }
   }
@@ -59,62 +34,9 @@ const changeBookNameMutation = gql`
   }
 `;
 
-const changeBookAuthorMutation = gql`
-  mutation changeBookAuthor($id: ID, $authorId: ID) {
-    updateBook(id: $id, authorId: $authorId) {
-      id
-      author {
-        id
-        name
-        email
-      }
-    }
-  }
-`;
-
-const changeAdditionalDetailsMutation = gql`
-  mutation changeAdditionalDetails(
-    $id: ID
-    $details: [AdditionalDetailsByBookInput]
-  ) {
-    updateBook(id: $id, additionalDetails: $details) {
-      id
-      additionalDetailsByBook {
-        id
-        year
-        pricePerUnit
-        totalSell
-      }
-    }
-  }
-`;
-
-const BookWithAuthorName = gql`
-  query BookWithAuthorName {
-    favoriteBook {
-      id
-      author {
-        name
-      }
-    }
-  }
-`;
-
-const BookWithAuthorBirthdate = gql`
-  query BookWithAuthorBirthdate {
-    favoriteBook {
-      id
-      author {
-        dateOfBirth
-      }
-    }
-  }
-`;
-
 const getBooksFromCache = () => {
   return client.readQuery({
     query: booksQuery,
-    debug: true,
   });
 };
 
@@ -137,37 +59,8 @@ const graphqlHOCConfig = {
   },
 };
 
-// const graphqlHOCConfigWithError = {
-//   name: 'getBooksWithError',
-//   skip: () => false,
-//   options: () => ({
-//     fetchPolicy: 'cache-only',
-//     // refetchWritePolicy: 'merge',
-//   }),
-//   props: ({ getBooksWithError }) => {
-//     // const data = getBooksFromCache();
-
-//     return {
-//       // books: getBooks?.books ?? [],
-//       // books: data?.books ?? [],
-//       // isLoading: [1, 2].includes(getBooksWithError.networkStatus),
-//       // isData: !!getBooks?.books,
-//       // isData: !!data?.books,
-//       refetch: getBooksWithError.refetch,
-//     };
-//   },
-// };
-
 const Books = ({ books, forceReRender, refetch }) => {
   const [changeBookName] = useMutation(changeBookNameMutation);
-
-  const [changeBookAuthor] = useMutation(changeBookAuthorMutation);
-
-  const [changeAdditionalDetails] = useMutation(
-    changeAdditionalDetailsMutation,
-  );
-
-  const [selectedBookId, setSelectedBookId] = React.useState('');
 
   return (
     <div>
@@ -181,30 +74,9 @@ const Books = ({ books, forceReRender, refetch }) => {
               await changeBookName({ variables: { id, newName } });
               forceReRender();
             }}
-            assignAuthor={({ id }) => setSelectedBookId(id)}
-            additionalDetailsByBook={book.additionalDetailsByBook}
-            changeAdditionalDetails={async ({ id, details }) => {
-              await changeAdditionalDetails({
-                variables: { id, details },
-              });
-
-              // await refetch();
-              forceReRender();
-            }}
           />
         </React.Fragment>
       ))}
-      {selectedBookId ? (
-        <AuthorsFeedWrapper
-          assignAuthor={async (authorId) => {
-            await changeBookAuthor({
-              variables: { id: selectedBookId, authorId },
-            });
-            setSelectedBookId('');
-            forceReRender();
-          }}
-        />
-      ) : null}
     </div>
   );
 };
--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -34,117 +34,10 @@ const BookEditDetails = ({ name, onCancelClick, onSaveClick }) => {
   );
 };
 
-const AdditionalDetails = ({ id, year, pricePerUnit, totalSell }) => {
-  return (
-    <div className="additionalDetails">
-      <div>
-        Year: <b>{year}</b>
-      </div>
-      <div>
-        Price per unit: <b>{pricePerUnit}</b>
-      </div>
-      <div>
-        Total sell: <b>{totalSell}</b>
-      </div>
-    </div>
-  );
-};
-
-const AddAdditionalDetails = ({ bookName, closeModal, onSaveClick }) => {
-  const [details, setDetails] = useState([
-    {
-      year: '',
-      pricePerUnit: '',
-      totalSell: '',
-    },
-    {
-      year: '',
-      pricePerUnit: '',
-      totalSell: '',
-    },
-  ]);
-
-  const updateDetails = (index, key, value) => {
-    setDetails((details) =>
-      details.map((detail, i) => {
-        if (i === index) {
-          return {
-            ...detail,
-            [key]: value,
-          };
-        }
-        return detail;
-      }),
-    );
-  };
-
-  return (
-    <div className="additionalDetailsInputModal">
-      <div className="additionalDetailsContent">
-        <div className="additionalDetailsHeader">
-          {bookName}
-          <button onClick={closeModal}>X</button>
-        </div>
-        <div className="additionalDetailsForm">
-          {_.map(details, (detail, index) => {
-            return (
-              <div className="additionalDetailsYearWiseInfo" key={index}>
-                <div className="inputFieldWrapper">
-                  year:
-                  <input
-                    type="text"
-                    value={detail.year}
-                    onChange={(e) =>
-                      updateDetails(index, 'year', e.target.value)
-                    }
-                  />
-                </div>
-                <div className="inputFieldWrapper">
-                  pricePerUnit:
-                  <input
-                    type="text"
-                    value={detail.pricePerUnit}
-                    onChange={(e) =>
-                      updateDetails(index, 'pricePerUnit', e.target.value)
-                    }
-                  />
-                </div>
-                <div className="inputFieldWrapper">
-                  totalSell:
-                  <input
-                    type="text"
-                    value={detail.totalSell}
-                    onChange={(e) =>
-                      updateDetails(index, 'totalSell', e.target.value)
-                    }
-                  />
-                </div>
-              </div>
-            );
-          })}
-          <button onClick={() => onSaveClick(details)}>Save</button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const Book = ({
-  name,
-  id,
-  author,
-  tags,
-  changeBookName,
-  assignAuthor,
-  additionalDetailsByBook,
-  changeAdditionalDetails,
-}) => {
+const Book = ({ name, id, author, changeBookName }) => {
   const [isEditing, setIsEditing] = useState(false);
 
   const [isLoading, setIsLoading] = useState(false);
-
-  const [isAddingAdditionalDetails, setIsAddingAdditionalDetails] =
-    useState(false);
 
   const onEditClick = () => {
     setIsEditing(true);
@@ -166,18 +59,6 @@ const Book = ({
     }
   };
 
-  const onChangeAdditionalDetailsLocal = async (details) => {
-    try {
-      setIsLoading(true);
-      await changeAdditionalDetails({ id, details });
-      setIsAddingAdditionalDetails(false);
-    } catch (e) {
-      console.error(e);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
     <div className="bookContainer">
       {!isEditing ? (
@@ -189,54 +70,7 @@ const Book = ({
           onSaveClick={onSaveClick}
         />
       )}
-      {author ? (
-        <Author name={author.name} email={author.email} />
-      ) : (
-        <button onClick={() => assignAuthor({ id })} className="assignAuthor">
-          Assign author
-        </button>
-      )}
-      {!_.isEmpty(tags) ? (
-        <div className="tagsContainer">
-          Tags:
-          <div className="tagsList">
-            {tags.map((tag) => (
-              <span key={tag.id} className="tag">
-                {tag.value}
-              </span>
-            ))}
-          </div>
-        </div>
-      ) : null}
-      {!_.isEmpty(additionalDetailsByBook) ? (
-        <div className="additionalInfoContainer">
-          <div className="additionalInfoContainerHeader">
-            Book performance in market since its publication
-          </div>
-          {_.map(additionalDetailsByBook, (additionalDetails) => (
-            <AdditionalDetails
-              id={additionalDetails.id}
-              year={additionalDetails.year}
-              pricePerUnit={additionalDetails.pricePerUnit}
-              totalSell={additionalDetails.totalSell}
-            />
-          ))}
-        </div>
-      ) : (
-        <button
-          onClick={() => setIsAddingAdditionalDetails(true)}
-          className="assignAuthor"
-        >
-          Add book performance details
-        </button>
-      )}
-      {isAddingAdditionalDetails ? (
-        <AddAdditionalDetails
-          bookName={name}
-          closeModal={() => setIsAddingAdditionalDetails(false)}
-          onSaveClick={onChangeAdditionalDetailsLocal}
-        />
-      ) : null}
+      {author ? <Author name={author.name} email={author.email} /> : null}
       {isLoading ? <FullScreenLoader /> : null}
     </div>
   );
